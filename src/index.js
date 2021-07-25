358e5d6a0b9f4545b9f320020bf11d2c329ab7d1
Natalia Sorokina <pticasonabird@gmail.com>
2021-07-25 23:43:27 +0300

    image acquisition done

--- src/index.js
+++ src/index.js
@@ -14,25 +14,7 @@ const refs = getRefs();
 
 refs.searchForm.addEventListener('submit', searchHandler);
 
-
-function renderImgCard(hits) {
-    refs.imgGallery.innerHTML = '';
-    // const { response: { hits }, response: { totalHits } } = response;
-       
-    if (hits.length === 0) {
-         Notiflix.Notify.failure(NOTIFICATION_FAILURE);
-    } else {
-       refs.imgGallery.innerHTML = imgCardTemplate(hits);
-    } 
-}
-
-
-function errorHandler(error) {
-    console.log(error);
-    Notiflix.Notify.failure(NOTIFICATION_FAILURE);
-}
-
-function searchHandler(event) {
+async function searchHandler(event) {
     event.preventDefault();
 
     const form = event.currentTarget;
@@ -45,7 +27,55 @@ function searchHandler(event) {
         return;
     }
 
-    API.fetchImages(inputQuery)
-        .then(renderImgCard)
-        .catch(errorHandler)
+    // API.fetchImages(inputQuery)
+    //     .then(renderImgCard)
+    //     .catch(errorHandler)
+    //     // .then((hits) => renderImgCard(hits))
+    //     // .catch(errorHandler);
+
+    try {
+        const data = await API.fetchImages(inputQuery);
+        console.log(data);
+        const { hits } = data;
+       return renderImgCard(hits);
+    } catch (error) {
+        console.log(error);
+        errorHandler(error);
+    }
+
 }
+
+
+function renderImgCard(hits) {
+    refs.imgGallery.innerHTML = '';
+    // const { response: { hits }, response: { totalHits } } = response;
+       
+    if (hits.length === 0) {
+        Notiflix.Notify.failure(NOTIFICATION_FAILURE);
+    } else {
+        refs.imgGallery.innerHTML = imgCardTemplate(hits);
+        //     const markup = { hits }
+        // .map(({ webformatURL, tags, likes, views, comments, downloads}) => {
+        //     return `
+        //   <div class="photo-card">
+        //     <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        //     <div class="info">
+        //         <p class="info-item"><b>Likes</b> ${likes}</p>
+        //         <p class="info-item"><b>Views</b> ${views}</p>
+        //         <p class="info-item"><b>Comments</b> ${comments}</p>
+        //         <p class="info-item"><b>Downloads</b> ${downloads}</p>
+        //     </div>
+        // </div>
+        // `;
+        // })
+        //         .join("");
+        //     refs.imgGallery.innerHTML = markup;
+        // } 
+    }
+}
+
+
+    function errorHandler(error) {
+        console.log(error);
+        Notiflix.Notify.failure(NOTIFICATION_FAILURE);
+    }
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,16 +1,26 @@
 import { BASE_URL, PERSONAL_KEY, SEARCH_PARAMS } from './constants';
 
-function fetchImages(inputQuery) {
+// function fetchImages(inputQuery) {
+//     const url = `${BASE_URL}/?key=${PERSONAL_KEY}&q=${inputQuery}&${SEARCH_PARAMS}`;
+//     console.log(url);
+//     return  fetch(url)
+//             .then(response => {
+//                 // console.log(response);
+//                 if (!response.ok) {
+//                     throw new Error(response.status);
+//                 }
+//                 return response.json();
+//             })
+// }
+
+
+ async function fetchImages(inputQuery) {
     const url = `${BASE_URL}/?key=${PERSONAL_KEY}&q=${inputQuery}&${SEARCH_PARAMS}`;
     console.log(url);
-    return  fetch(url)
-            .then(response => {
-                // console.log(response);
-                if (!response.ok) {
-                    throw new Error(response.status);
-                }
-                return response.json();
-            })
+     const response = await fetch(url);
+     const newImages = await response.json();
+
+     return newImages;
 }
 
 export default { fetchImages };
